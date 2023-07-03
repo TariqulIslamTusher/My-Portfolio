0db11593c075e9bd6b2252ec0bdc92e49d986cdf
TariqulIslamTusher <islamtariqul652@gmail.com>
2023-07-03 22:32:49 +0600

    added the arrow button to the banner

--- src/Components/HomeCompo/Banner/Banner.jsx
+++ src/Components/HomeCompo/Banner/Banner.jsx
@@ -6,15 +6,21 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 
 import myImg from '../../../assets/MyImg/myImgBgless.jpg'
+import { FaArrowDown } from 'react-icons/fa';
 
 
 
 const Banner = () => {
     return (
         <div id='banner' className='bg-slate-300 w-full lg:flex items-center mx-auto justify-around'>
-            <div className='lg:static absolute !lg:w-8/12 left-10 top-1/3 inset-75'>
-                <h1 className="text-4xl">Hi, I m Tariqul</h1>
+            <div className='lg:static absolute !lg:w-8/12 left-10 top-1/3 inset-75 leading-4'>
+                <h1 className="text-4xl font-bold text-black">Hi!</h1>
+                <h1 className="text-4xl font-bold text-black">I m Tariqul</h1>
                 <h2 className="text-2xl text-yellow-700">MERN Stack web developer</h2>
+
+                <FaArrowDown className='text-4xl relative text-center right-0 bottom-0'></FaArrowDown>
+
+
             </div>
             <img className='' src={myImg} alt="" />
         </div>
